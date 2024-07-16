56b735dfb6e3ce9e2997fe9b3864a644495ddf3d
Tonn3y <124000476+Tonn3y@users.noreply.github.com>
2024-07-16 12:52:55 +0300

    Add files via upload
    A simple drum kit.

--- index.js
+++ index.js
@@ -1,20 +1,52 @@
-let randomNumber1 = Math.floor (Math.random() * 6) + 1;
-console.log(randomNumber1);
-let randomdiceimage = "dice" + randomNumber1 + ".png"
-let randomimagesource = "images/" + randomdiceimage
-let image1 = document.querySelectorAll("img")[0]
-image1.setAttribute("src", randomimagesource)
 
-let randomnumber2 = Math.floor (Math.random() * 6) + 1;
-let randomimagesource2 = "images/dice" + randomnumber2 + ".png"
-document.querySelectorAll("img")[1].setAttribute("src", randomimagesource2)
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
-if (randomNumber1 > randomnumber2) {
-    document.querySelector("h1").innerHTML = "PLAYER ONE WINS"
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio("sounds/tom-2.mp3");
+                tom2.play();
+                break;
+
+            case "s":
+                var tom3 = new Audio("sounds/tom-3.mp3");
+                tom3.play();
+                break;
+
+            case "d":
+                var tom4 = new Audio("sounds/tom-4.mp3");
+                tom4.play();
+                break;
+
+            case "j":
+                var snare = new Audio("sounds/snare.mp3");
+                snare.play();
+                break;
+
+            case "k":
+                var crash = new Audio("sounds/crash.mp3");
+                crash.play();
+                break;
+
+            case "l":
+                var kick = new Audio("sounds/kick-bass.mp3");
+                kick.play();
+                break;
+
+            default: console.log(buttonInnerHTML)
+        }
+    })
+
+
 }
-else if (randomnumber2 > randomNumber1){
-    document.querySelector("h1").innerHTML = " PLAYER TWO WINS "
-}
-else {
-    document.querySelector("h1").innerHTML = "DRAW"
-}
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();
